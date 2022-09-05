bbf1926ed3a754d038b7caeca6e0a57598f80019
bayhaqibnu <bayhaqiibnu8@gmail.com>
2022-09-05 08:50:49 +0700

    menambahkan banyak mode pada website

--- src/script.js
+++ src/script.js
@@ -1,3 +1,7 @@
+// =====================================================================================================
+// ======================================== cara ribet by me ===========================================
+// =====================================================================================================
+
 let root = document.getElementById('wrapper-content');
 
 let allParagraf = document.querySelectorAll('p');
@@ -7,51 +11,49 @@ let link = root.querySelectorAll('a');
 let icon = root.querySelectorAll('i');
 
 
-for(let i = 0; i < link.length; i++) {
-    link[i].addEventListener('mouseenter', function () {
-        icon[i].classList.toggle('change');
-    });
+// for(let i = 0; i < link.length; i++) {
+//     link[i].addEventListener('mouseenter', function () {
+//         icon[i].classList.toggle('change');
+//     });
 
-    link[i].addEventListener('mouseleave', function () {
-        icon[i].classList.remove('change');
-    });
-};
+//     link[i].addEventListener('mouseleave', function () {
+//         icon[i].classList.remove('change');
+//     });
+// };
 
-function mode(isDark){
-    if(isDark == 'true'){
-        document.body.style.backgroundColor='rgba(0,0,0,0.7)';
-        document.body.style.color='white';
-        for(let i=0; i<allParagraf.length; i++){
-            allParagraf[i].style.color='white';
-        }
-        for(let i=0; i<allArrow.length; i++){
-            allArrow[i].style.color = 'white';
-        }
+// function mode(isDark){
+//     if(isDark == true){
+//         document.body.style.backgroundColor='rgba(0,0,0,0.7)';
+//         document.body.style.color='white';
+//         for(let i=0; i<allParagraf.length; i++){
+//             allParagraf[i].style.color='white';
+//         }
+//         for(let i=0; i<allArrow.length; i++){
+//             allArrow[i].style.color = 'white';
+//         }
+//     }else {
+//         document.body.style.backgroundColor='rgba(0,0,0,0.01)';
+//         document.body.style.color='black';
+//         for(let i=0; i<allParagraf.length; i++){
+//             allParagraf[i].style.color='black';
+//         }
+//     }
+// }
+
+// =====================================================================================================
+// ======================================== cara simple by kang hilman =================================
+// =====================================================================================================
+
+function mode(isDark) {
+    if(isDark == 'moon') {
+        document.body.setAttribute('id', 'darkmode');
+    }else if(isDark == 'sun'){
+        document.body.setAttribute('id', 'lightmode');
+    }else if(isDark == 'angry'){
+        document.body.setAttribute('id', 'angry');
     }else {
-        document.body.style.backgroundColor='rgba(0,0,0,0.01)';
-        document.body.style.color='black';
-        for(let i=0; i<allParagraf.length; i++){
-            allParagraf[i].style.color='black';
-        }
+        alert('Mohon maaf, mode sedang dalam pengembangan!')
     }
 }
 
 
-
-// for(let i = 1; i <= link.length; i++) {
-//     console.log(`test ${i}`);
-// }
-
-// root.forEach(function(el) {
-//     el.addEventListener('mouseenter', function() {
-//         for(let i = 0; i < textContent.length; i++) {
-//                 console.log('test cuy');
-//         }
-//     });
-// });
-
-
-
-// console.log(link[0]);
-// console.log(textContent[0]);
-// console.log(icon[0]);
